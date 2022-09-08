4db31d0ff8c8ea9bcc3b30f054a105430a3c46c3
Stephen <Stephok5@gmail.com>
2022-09-08 19:08:03 +0000

    All data types size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("Size of char: %i bytes\n", sizeof(char));
-printf("Size of int: %i bytes\n", sizeof(int));
-printf("Size of long int: %i bytes\n", sizeof(long int));
-printf("Size of long long int: %i bytes\n", sizeof(long long int));
-printf("Size of float: %i bytes\n", sizeof(float));
+printf("Size of char: %i byte(s)\n", sizeof(char));
+printf("Size of int: %i byte(s)\n", sizeof(int));
+printf("Size of long int: %i byte(s)\n", sizeof(long int));
+printf("Size of long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of float: %i byte(s)\n", sizeof(float));
 return (0);
 }
