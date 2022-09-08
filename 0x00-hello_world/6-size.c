f5bf8670edfc33048d47e9bdffdbca5607b72d7b
Stephen <Stephok5@gmail.com>
2022-09-08 18:36:04 +0000

    Size of Types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-char cahrType;
+char charType;
 int intType;
 long intType;
 long longintType;
 float floatType;
 
-printf("Size of char: %zu bytes\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long: %zu bytes\n", sizeof(intType));
-printf("Size of long: %zu bytes\n", sizeof(longintType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of char: %lu bytes\n", sizeof(charType));
+printf("Size of int: %lu bytes\n", sizeof(intType));
+printf("Size of long: %lu bytes\n", sizeof(intType));
+printf("Size of long: %lu bytes\n", sizeof(longintType));
+printf("Size of float: %lu bytes\n", sizeof(floatType));
 return (0);
 }
