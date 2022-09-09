abba468cb1afe05be84e7bcffd5d3d2001e7f97f
Stephen <Stephok5@gmail.com>
2022-09-09 15:35:38 +0000

    Random Positive of negative numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if(n>0){
-		printf("n is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n<0){
-			printf("n is negative\n");
+			printf("d% is negative\n", n);
 	}
-return (n);
+	else 
+		printf("%d is zero\n", n);
+return (0);
+
 }
