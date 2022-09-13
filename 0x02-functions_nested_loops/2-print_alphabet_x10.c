c55950882c8ac9d078e14a42886e580d0470c1e8
Stephen <Stephok5@gmail.com>
2022-09-13 16:14:16 +0000

    Alphabets ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 {
 	char x, i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-        for(x = 'a'; x <= 'z'; x++)
-        {
-            _putchar(x);
+		for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
         }
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
     
 }
