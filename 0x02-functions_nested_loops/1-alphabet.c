dfbe46fe2af71ded939359b373ae3c2d37aa813a
Stephen <Stephok5@gmail.com>
2022-09-13 14:30:37 +0000

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,12 @@
 
 void print_alphabet(void)
 {
-    char x;
-    
-    for (x = 'a'; x <= 'z'; x++)
-    {
-        _putchar(x);
-    }
-    _putchar('\n');
+	char x;
+	
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	
+	_putchar('\n');
 }
