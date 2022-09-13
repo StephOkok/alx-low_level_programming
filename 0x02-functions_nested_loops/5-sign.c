96b2f46423f71c2607b1e136f78cb3e2eab70c90
Stephen <Stephok5@gmail.com>
2022-09-13 20:45:48 +0000

    Print positive and negative sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
  * print_sign - function to print sign of positive or negative
- * @n - interger fro argument
+ * @n- interger for argument if it is positive or negative
  * Return: 0 always success
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return(1);
+		_putchar ('+');
+		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
-		return(-1);
+		_putchar ('-');
+		return (-1);
 	}
 	else
-		_putchar('0');
-	return(0);
+		_putchar ('0');
+	return (0);
 }
