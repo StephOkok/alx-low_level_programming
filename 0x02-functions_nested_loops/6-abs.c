22e33bb18d53ee1757f70a152047ea3868dbd44f
Stephen <Stephok5@gmail.com>
2022-09-13 20:59:30 +0000

    Print absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - function that prints absolute integer
+ * @int: print absolute vatue
+ * Return: 0 always when successful
+ */
+
+int _abs(int y)
+{
+	if (y > 0 || y -- 0)
+	{
+		return (y);
+	}
+	else
+		return (c * -1);
+}
