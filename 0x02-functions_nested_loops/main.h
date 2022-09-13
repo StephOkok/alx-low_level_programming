c1f1008960b6fc33df868ccc3432d2664d66d61f
Stephen <Stephok5@gmail.com>
2022-09-13 13:49:25 +0000

    Print _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,4 +13,3 @@ void print_t0_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
-#endif
