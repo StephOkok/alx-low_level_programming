f353612869713bf07cbe4b97ffe5e98e0d12d097
Stephen <Stephok5@gmail.com>
2022-09-14 17:23:11 +0000

    Printing multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print count of multiples
+ * of 3/5/below 1024
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n, sum = 0;
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+			sum += n;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
