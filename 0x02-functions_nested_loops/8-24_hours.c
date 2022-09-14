5b9d6e4a321e6ae1aa155d53da2c1daa993696e5
Stephen <Stephok5@gmail.com>
2022-09-14 10:12:52 +0000

    Jack Bauer timer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,17 +15,18 @@ void jack_bauer(void)
         while (hours <= 23)
         {
                 while (minutes <= 59)
-                {
-                hours_remainder = hours % 10;
-                _putchar (hours / 10 + '0');
-                _putchar (hours_remainder + '0');
-                _putchar (':');
-                _putchar (minutes / 10 + '0');
-                _putchar (mins_remainder + '0');
-                minutes++;
-                _putchar ('\n');
+		{
+			mins_remainder = minutes % 10;
+                	hours_remainder = hours % 10;
+                	_putchar (hours / 10 + '0');
+                	_putchar (hours_remainder + '0');
+                	_putchar (':');
+                	_putchar (minutes / 10 + '0');
+                	_putchar (mins_remainder + '0');
+                	minutes++;
+                	_putchar ('\n');
                 }
-                hours++;
-		minutes = 0;
+                	hours++;
+			minutes = 0;
         }
 }
