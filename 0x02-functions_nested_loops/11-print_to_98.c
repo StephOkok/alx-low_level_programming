acc3f75cdc6c775b3d5aa08c5cb03bb6ad512028
Stephen <Stephok5@gmail.com>
2022-09-14 14:50:56 +0000

    Print to 98 from integer

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_to_98 - fuction to print from an integer to 98
+ * @n: interger which is the first count
+ * Return: 0 always success
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
