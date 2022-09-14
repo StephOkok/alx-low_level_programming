366f4b4883ae974ba9cea37a1c8ef3c1c80bae01
Stephen <Stephok5@gmail.com>
2022-09-14 17:40:37 +0000

    Fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - output sum of finobacci even Nums
+ * fibonacci suit numbers
+ * Return: 0 always success
+ */
+int main(void)
+{
+        unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+
+        while (n3 <= 4000000)
+        {
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                if ((n1 % 2) == 0)
+                        sum +=n1;
+        }
+        printf("%ld\n", sum);
+        return (0);
+}
