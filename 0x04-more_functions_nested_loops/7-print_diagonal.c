2ed9942685d95b1ae989a2c2b78a6c3d1a5d494f
Stephen <Stephok5@gmail.com>
2022-09-15 22:14:53 +0000

    Draw lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,13 +2,9 @@
 #include "main.h"
 
 /**
-*print_diagonal - prints diagonal line
-*@n: parameter
-<<<<<<< HEAD
-Return: return nothing
-=======
-Return: return 0
->>>>>>> b8910a0a684535c84f4fe2ac89f9e7f2c3f23ac5
+* print_diagonal - prints diagonal line
+* @n: parameter
+* Return: return nothing
 */
 
 void print_diagonal(int n)
