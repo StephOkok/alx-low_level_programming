c1fa937fa326f7204d40bc828fdf96d50fd7ce67
Stephen <Stephok5@gmail.com>
2022-09-15 19:34:44 +0000

    Check if number is a digit from 0 - 9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,8 +3,7 @@
 /**
  * _isdigit - function to check if an integer is a digit 0-9
  * @c: integer used for checking if is 0-9
- * Return: 1 if true
- * Return: 0 if false
+ * Return: 1 if true 0 if false
  */
 
 int _isdigit(int c)
