76019e9714cc391e476c77e812489b6fc4057585
Stephen <Stephok5@gmail.com>
2022-09-27 20:38:54 +0000

    Funtion to print a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_chessboard - a fucntion to print a chess board
+ * @a: one dimention of the chessboard
+ * Return: 0 always success
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	return (0);
+}
