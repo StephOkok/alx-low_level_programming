9ac2ce8e8c99bf3c074b15c588f7b3ebbe665a63
Stephen <Stephok5@gmail.com>
2022-09-28 21:20:44 +0000

    a function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,11 +7,24 @@
  */
 int is_prime_number(int n)
 {
-	if (n < 1)
+	if (n < 0 || n == 1)
 	{
 		return (0);
 	}
 	else if (n % 2 == 0 && n % 3 == 0)
+	{
+		return (0);
+	}
+	else if (n % 2 == 0 && n % 3 != 0)
+	{
+		return (0);
+	}
+	else if (n % 2 != 0 && n % 3 == 0)
+	
+	{
+		return (0);
+	}
+	else if (n % 2 != 0 && n % 3 != 0)
 	{
 		return (1);
 	}
