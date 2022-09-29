fa20edf5f692201bfab20505cd4860dfcf8e1ae5
Stephen <Stephok5@gmail.com>
2022-09-29 18:49:10 +0000

    a function that returns 1 if a string is a palindrome and 0 if not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * is_palindrome - a function that returns 1 if a string is a palindrome
+ * and 0 if not
+ * @s: string to check
+ * Return: 1 if a string is a palindrome and 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int n = strlen(s);
+
+	if (n == 0)
+		return (1);
+	return (is_palindrome(s - 1));
+}
+
+/**
+ * pal - function to check if string is palindrome or not
+ * @s: string
+ * @a: first character of the string
+ * @z: last character of the string
+ * Return: 1 if palindrome and 0 if not
+ */
+
+int pal(char *s, int a, int z)
+{
+	if (a == z)
+		return (1);
+	if (s[a] != s[z])
+		return (0);
+	if (a < z + 1)
+		return (1);
+	if (s[a] - 1 != s[z] + 1)
+		return (0);
+	return (pal(s, a + 1, z - 1));
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,19 +8,22 @@
 
 int _sqrt_recursion(int n)
 {
-	int i = 1;
-
-	if (i * i == n)
-	{
-		return (i);
-	}
-	else if (i * i != n)
-	{
-		return (-1);
-	}
-	else
-	{
-		i += 1;
-	}
+	if (n == 1 || n == 0)
+		return (n);
 	return (_sqrt_recursion(n));
+}
+
+/**
+ * _sqrt_recursion -  a function that returns the
+ * natural square root of a number
+ * @n: integer to check the squareroot
+ * Return: the squareroot of the number or -1 if there is no
+ */
+int _sqrt(int n, int i)
+{
+	if (n > i / 2)
+		return (-1);
+	else if (n * n == i)
+		return (n);
+	return (_sqrt(n + 1, i));
 }
