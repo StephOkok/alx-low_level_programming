33ea0f08e1a1a141c72bfde18a95845daa3c56d2
Stephen <Stephok5@gmail.com>
2022-10-03 12:02:08 +0000

    A function that prints an array of chars, and initializes it with a specific char Returns NULL if size = 0 Returns a pointer to the array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
-	p = malloc(sizeof(char) * (size + 1));
+	p = malloc(sizeof(char) * size);
 	if (size == 0)
 	{
 		return (0);
