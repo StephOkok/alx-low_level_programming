da405b6505269c85fd7ddaf2a1f6368be426d3b6
Stephen <stephok5@gmail.com>
2022-10-10 11:17:05 +0000

    Print to 98

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
-void print_t0_98(int n);
+void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
